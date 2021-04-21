df694e1abe0456a90b749119c7fd94e13f84adbe
Lioo7 <lioz.akirav@gmail.com>
2021-04-21 05:05:34 +0300

    fix bugs in test file

--- Test.cpp
+++ Test.cpp
@@ -11,15 +11,15 @@ using std::istringstream;
 using std::string;
 using namespace std;
 using namespace ariel;
-const string TRUE = "true"; 
+const string TRUE = "true";
 const string FALSE = "false";
 
 NumberWithUnits a{1, "km"};     // 1km = 1,000m = 100,000cm
 NumberWithUnits b{200, "m"};    // 0.2km = 200m = 20,000cm
-NumberWithUnits c{30, "c"};     // 0.0003km = 0.3m = 30cm
+NumberWithUnits c{30, "cm"};    // 0.0003km = 0.3m = 30cm
 NumberWithUnits a_n{-1, "km"};  // -1km = -1,000m = -100,000cm
 NumberWithUnits b_n{-200, "m"}; // -0.2km = -200m = -20,000cm
-NumberWithUnits c_n{-30, "c"};  // -0.0003km = -0.3m = -30cm
+NumberWithUnits c_n{-30, "cm"}; // -0.0003km = -0.3m = -30cm
 
 // ifstream units_file{"units.txt"}; //TODO: fix this test
 // TEST_CASE("adds units from a text file")
@@ -44,20 +44,26 @@ TEST_CASE("checks basic arithmetic operators on distance elements")
     CHECK(b - c == NumberWithUnits(199.7, "m"));
     CHECK(c - b == NumberWithUnits(19070, "cm"));
     // +=
-    CHECK(a += NumberWithUnits{2, "km"} == NumberWithUnits(3, "km"));
-    CHECK(b += NumberWithUnits{100, "m"} == NumberWithUnits(300, "m"));
-    CHECK(c += NumberWithUnits{20, "cm"} == NumberWithUnits(50, "cm"));
+    a += NumberWithUnits{2, "km"};
+    CHECK(a == NumberWithUnits(3, "km"));
+    b += NumberWithUnits{100, "m"};
+    CHECK(b == NumberWithUnits(300, "m"));
+    c += NumberWithUnits{20, "cm"};
+    CHECK(c == NumberWithUnits(50, "cm"));
     // -=
-    CHECK(a -= NumberWithUnits{3, "km"} == NumberWithUnits(2, "km"));
-    CHECK(b -= NumberWithUnits{300, "m"} == NumberWithUnits(100, "m"));
-    CHECK(c -= NumberWithUnits{50, "cm"} == NumberWithUnits(20, "cm"));
+    a -= NumberWithUnits{3, "km"};
+    CHECK(a == NumberWithUnits(2, "km"));
+    b -= NumberWithUnits{300, "m"};
+    CHECK(b == NumberWithUnits(100, "m"));
+    c -= NumberWithUnits{50, "cm"};
+    CHECK(c == NumberWithUnits(20, "cm"));
 }
 
 TEST_CASE("checks basic arithmetic operators on mixed elements")
 {
     NumberWithUnits d_a{1, "km"};
     NumberWithUnits d_b{200, "m"};
-    NumberWithUnits d_c{30, "c"};
+    NumberWithUnits d_c{30, "cm"};
     NumberWithUnits w_a{1, "ton"};
     NumberWithUnits w_b{200, "kg"};
     NumberWithUnits w_c{30, "g"};
@@ -77,13 +83,13 @@ TEST_CASE("checks basic arithmetic operators on mixed elements")
     CHECK_THROWS(w_b - d_c);
     CHECK_THROWS(w_c - d_b);
     // +=
-    CHECK_THROWS(d_a += w_a); 
+    CHECK_THROWS(d_a += w_a);
     CHECK_THROWS(d_b += w_b);
     CHECK_THROWS(d_c += w_c);
     // -=
-    CHECK_THROWS(w_a -= w_a);
-    CHECK_THROWS(w_b -= w_b);
-    CHECK_THROWS(w_c -= w_c);
+    CHECK_THROWS(d_a -= w_a);
+    CHECK_THROWS(d_b -= w_b);
+    CHECK_THROWS(d_c -= w_c);
 }
 
 TEST_CASE("unary operators")
@@ -131,16 +137,25 @@ TEST_CASE("checks basic equality operators on mixed elements")
 TEST_CASE("pre/post fix")
 {
     // pre
-    CHECK((++a) == 2);
-    CHECK((++b) == 201);
-    CHECK((--a) == 1);
-    CHECK((--b) == 200);
+    NumberWithUnits temp{1, "km"};
+    temp = ++a; 
+    CHECK(temp == NumberWithUnits{2, "km"});
+    temp = ++b; 
+    CHECK(temp == NumberWithUnits{201, "m"});
+    temp = --a; 
+    CHECK(temp == NumberWithUnits{1, "km"});
+    temp = --b; 
+    CHECK(temp == NumberWithUnits{200, "m"});
 
     // post
-    CHECK((a++) == 2);
-    CHECK((b++) == 201);
-    CHECK((a--) == 1);
-    CHECK((b--) == 200);
+    temp = a++; 
+    CHECK(temp == NumberWithUnits{1, "km"});
+    temp = b++; 
+    CHECK(temp == NumberWithUnits{200, "m"});
+    temp = a--; 
+    CHECK(temp == NumberWithUnits{2, "km"});
+    temp = b--; 
+    CHECK(temp == NumberWithUnits{201, "m"});
 }
 
 TEST_CASE("multiplication operator")
@@ -148,32 +163,32 @@ TEST_CASE("multiplication operator")
     // direction: ->
     CHECK(a * 2 == NumberWithUnits(2, "km"));
     CHECK(b * 1 == NumberWithUnits(200, "m"));
-    CHECK(c * 10.5 == NumberWithUnits(450, "cm"));
+    CHECK(c * 10.5 == NumberWithUnits(315, "cm"));
     // direction: <-
     CHECK(2 * a == NumberWithUnits(2, "km"));
     CHECK(1 * b == NumberWithUnits(200, "m"));
-    CHECK(10.5 * c == NumberWithUnits(450, "cm"));
+    CHECK(10.5 * c == NumberWithUnits(315, "cm"));
 }
 
-TEST_CASE("I/O operators")  
+TEST_CASE("I/O operators")
 {
     // input
-    NumberWithUnits input{0, "none"};
+    NumberWithUnits input{0, "none"};  //TODO: have to check
     istringstream my_stream_1("1[km]");
     my_stream_1 >> input;
     CHECK(input == a);
 
     NumberWithUnits input_2(0, "none");
     istringstream my_stream_2("200 [ m ]");
-    my_stream_2 >> input;
-    CHECK(input == b);
+    my_stream_2 >> input_2;
+    CHECK(input_2 == b);
 
     // output
     ostringstream output;
     output << a;
-    CHECK(output == "1[km]");
+    CHECK(output.str() == "1[km]");
 
     ostringstream output2;
     output2 << b;
-    CHECK(output2 == "200[m]");
+    CHECK(output2.str() == "200[m]");
 }
